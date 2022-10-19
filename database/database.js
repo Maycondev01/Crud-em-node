475db1e02e9f237b0a0b39993b0f23c0d4bef73e
Maycondev01 <mayconlundgren186@gmail.com>
2022-10-18 21:26:24 -0300

    Banco de dados

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('guiapress','root','182122', {
     host: 'localhost',
     dialect: 'mysql',
-    logging: false
+    logging: false,
+    timezone: "-03:00"
 })
 
 module.exports = connection;
