f6ba3011fd2a886e54689c5804c6f2cc3718a2c9
Maycondev01 <mayconlundgren186@gmail.com>
2022-10-18 21:26:49 -0300

    Arquivo Raiz

--- index.js
+++ index.js
@@ -9,33 +9,79 @@ const Category = require("./categories/Category")
 
 // DataBase 
 connection
-.authenticate()
-.then(() => {
-    console.log("Database Connected")
-}).catch((err) => {
-    console.log(err)
-})
+    .authenticate()
+    .then(() => {
+        console.log("Database Connected")
+    }).catch((err) => {
+        console.log(err)
+    })
 
 // VIEW ENGINE
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
 // Body Parser
-app.use(bodyParser.urlencoded({ extended: false})); // Aceitar dados de formulário
+app.use(bodyParser.urlencoded({ extended: false })); // Aceitar dados de formulário
 app.use(bodyParser.json())  // Aceitar dados JSON.
 
 //Arquivos Static
 app.use(express.static('public'));
 
-app.get("/", function(req,res) {
-    res.render("index")
+app.get("/", function (req, res) {
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", { articles: articles, categories: categories });
+        })
+    });
 })
+
+app.get("/:slug", function (req, res) {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: { slug: slug }
+    }).then(article => {
+        if (article != undefined) {
+            Category.findAll().then(categories => {
+                res.render("article", { article: article, categories: categories });
+            });
+        } else {
+            res.redirect("/");
+        }
+    }).catch((e) => {
+        res.redirect("/")
+    })
+})
+
+
+app.get("/category/:slug", function (req, res) {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{ model: Article }] // Join | Juntar: Quando o Sequelize busca uma Categoria Inclua os Artigos
+    }).then(category => {
+        if (category != undefined) {
+            Category.findAll().then(categories => {
+                res.render("index", { articles: category.articles, categories: categories }) // Só conseguimos acessar por conta do include ^
+            });
+        } else {
+            res.redirect("/")
+        }
+    }).catch(e => {
+        res.redirect("/")
+    });
+});
 
 app.use("/", CategoriesController);
 
 app.use("/", ArticlesController);
 
-app.listen(8080, function(err) {
-    if(err) {
+app.listen(8080, function (err) {
+    if (err) {
         console.log(err)
     } else {
         console.log('Servidor Online!')
